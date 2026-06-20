8f79753c1d426968606a881fb052ae74c1cb7d61
shrreyasethu <sethura0@purdue.edu>
2026-06-20 06:45:48 -0400

    description for navbar

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,6 +1,13 @@
 import {Link} from "react-router-dom"
 import "../css/Navbar.css"
 
+/*
+  We use the Link element: when user clicks on a certain name (Home, Favorites, etc.),
+  then the Link element allows programmer to decide which page (URL) I should direct the user to.
+  I have already definitions to each path in App.jsx in <Routes></Routes>
+  For example, if user clicks on "Favorites", then it will direct them to Favorites page (http://localhost/favorites)
+*/
+
 function NavBar() {
     return (
         <nav className = "navbar">
